d252aff561592e7ffd5ae1a4e98db20595718bdb
code-danil <quantumfoge@gmail.com>
2026-01-09 20:38:50 +0600

    lessons2

--- lessons3.py
+++ lessons3.py
@@ -9,8 +9,6 @@ class Car:
     def drive_to(self, destination):
         print(f"Car {self.model} Driving to", destination)
 
-    def test(self):
-        pass
 
 # дочерний, подклас
 class Bus(Car):
@@ -18,17 +16,16 @@ class Bus(Car):
         super().__init__(model, color)
         self.number = number
 
+class Truck(Car):
     def drive_to(self, destination):
-        super().drive_to(destination)
-        print(f"Bus {self.number} Driving to", destination)
+        print('Truck driving to', destination)
 
-bus_42 = Bus("Ikarus", "green", 42)
-print(bus_42.number)
-print(bus_42.speed)
-bus_42.drive_to("Bishkek")
-print(type(bus_42))
-print(isinstance(bus_42, Bus))
-print(isinstance(bus_42, Car))
-print(type(bus_42))
-if type(bus_42) == 'Bus':
-    print("object is Bus")
+
+bus_42 = Bus("Mercedes", "red", 42)
+truck = Truck('MAN', 'white')
+car_subaru = Car('subaru', 'red')
+vehicles = (bus_42, truck, car_subaru)
+for v in vehicles:
+    v.drive_to('Bishkek')
+
+
